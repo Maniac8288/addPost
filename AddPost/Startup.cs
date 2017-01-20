66fb8aaf42a96100e147c65d3e322f336424d6a5
Maniac Звинаревский <maks-bot-5@mail.ru>
2017-01-20 23:29:56 +0200

    Добавьте файлы проекта.

---
+++ AddPost/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AddPost.Startup))]
+namespace AddPost
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
